4582e6c1bdb25e3cd5cbed165937766683bc3892
LimeEcho <yiguo.xfy@icloud.com>
2025-01-10 16:52:58 +0800

    alpha 1.5

--- headers/vec3.h
+++ headers/vec3.h
@@ -12,12 +12,6 @@
 #include <stdlib.h>
 #include <math.h>
 
-typedef struct vec_usg {
-	void *add;
-	char state;
-	struct vec_usg *next;
-} vec_usg;
-
 float *req (float, float, float);				// 获取，在C++里用class，但是我就是喜欢C！(♯｀∧´)
 void vfree (float *);
 float rx (float *);
